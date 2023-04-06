2813db80acc92b31fb3b57f181e1ea6a98f823cf
Pro100-Almaz <almaz.amanzholuly@nu.edu.kz>
2023-04-06 23:43:29 +0600

    Added: main field component

---
+++ src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main-field">asdfasd</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.main-field {
+  padding: 1rem 1rem;
+  font-size: x-large;
+}
+</style>
