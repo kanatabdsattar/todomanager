d6373f4264f20cd025e5e66266cdd620757b6bd9
kanat <abdsattar02@inbox.ru>
2023-04-20 23:14:54 +0600

    feature(count down component)

---
+++ src/views/TaskList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="app">
+        <count-down/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import CountDown from "@/components/CountDown.vue";
+</script>
+
+<style scoped> 
+.app{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
