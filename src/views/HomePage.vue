187b85ca7be6481020a0e63e724fa52be3e61fdd
almaz <almaz.amanzholuly@nu.edu.kz>
2023-04-23 21:52:34 +0600

    Fixed: some in main file

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,37 +9,6 @@
       </div>
     </div>
     <AddTask @add="addTodo" class="input-text-block" />
-      <Sidebar
-        v-model:visible="visibleRight"
-        position="right"
-        class="right-sidebar"
-        style="
-        background-color: var(--color-background-sidebar); 
-        color: white; 
-        border-radius: 0.5rem;
-        width: 30%;
-        height: 95%;
-        margin-right: 1rem;"
-      >
-        <div class="title">
-
-        </div>
-        <input type="text" v-model="todoTitle" class="title-text" />
-        <p class="p-float-label">
-          <Textarea
-            id="value"
-            v-model="todoContent"
-            rows="2"
-            cols="43"
-            aria-describedby="text-error"
-            style="background-color: var(--color-background-sidebar);
-            border: none;
-            outline: none;
-            border-bottom: 1px solid gray;"
-          />
-          <label for="value">Description</label>
-        </p>
-      </Sidebar> 
   </div>
 </template>
 
